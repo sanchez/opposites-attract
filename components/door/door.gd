951a269584bf00e845267101dd9f18b559e50429
sanchez <dfitz360@gmail.com>
2022-11-26 15:08:47 +1000

    got the door stuff working

--- components/door/door.gd
+++ components/door/door.gd
@@ -10,9 +10,11 @@ onready var ClosedShapeNode := $ClosedShape
 
 export (NodePath) var SWITCH
 
+onready var SwitchNode = get_node(SWITCH)
+
 
 func _process(delta):
-	if SWITCH.unlocked():
+	if SwitchNode.unlocked():
 		SpriteNode.texture = DoorOpenTexture
 		ClosedShapeNode.disabled = true
 	else:
--- components/switch/switch.gd
+++ components/switch/switch.gd
@@ -8,5 +8,33 @@ const PositiveIcon := preload("res://components/magnet/positive.png")
 const NegativeIcon := preload("res://components/magnet/negative.png")
 
 
+onready var ChargeIconNode := $ChargeIcon
+
+
+func get_total_charge():
+	var charge_total = 0.0
+	
+	for x in get_overlapping_areas():
+		if x is Charge:
+			var dir = global_position - x.global_position
+			var dist = dir.length_squared()
+			var power = clamp(1_000_000 / dist, 0, 5_000) * x.STRENGTH
+			var charge_type = x.CHARGE
+			var charge_power = power * charge_type * CHARGE
+			charge_total += charge_power
+	
+	return charge_total
+
+
 func unlocked():
-	return false
+	var charge_total = get_total_charge()
+	return charge_total <= -1
+
+
+func _process(delta):
+	if CHARGE == Charge.CHARGE_TYPE.NEGATIVE_CHARGE:
+		ChargeIconNode.texture = NegativeIcon
+	elif CHARGE == Charge.CHARGE_TYPE.POSITIVE_CHARGE:
+		ChargeIconNode.texture = PositiveIcon
+	else:
+		ChargeIconNode.texture = null
